ee3a9c472a0312c42284c25427ee457d2bb79679
shuliqi <shuliqi@360.cn>
2021-06-05 20:36:30 +0800

    cors

--- serve/index.js
+++ serve/index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//设置跨域访问
+app.all('*', function(req, res, next) {
+  //  响应头添加 Access-Control—Allow-Origin
+  res.header("Access-Control—Allow-Origin", "http://localhost:8080/ ")
+  next();
+});
+
 app.get('/api/getName', (req, res) => {
   console.log("请求到后端了")
   res.send('舒丽琦')
