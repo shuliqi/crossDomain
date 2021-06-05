9112479b5380d353b1631f311bd61c973bbef681
shuliqi <shuliqi@360.cn>
2021-06-05 21:22:04 +0800

    cors

--- serve/index.js
+++ serve/index.js
@@ -4,8 +4,15 @@ const port = 3000
 
 //设置跨域访问
 app.all('*', function(req, res, next) {
-  //  响应头添加 Access-Control—Allow-Origin
-  res.header("Access-Control—Allow-Origin", "http://localhost:8080/ ")
+  // 设置哪个源可以访问我
+  res.header("Access-Control-Allow-Origin", "*");
+
+  // 允许携带哪个头访问我
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+   // 允许哪个方法访问我
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+
   next();
 });
 
